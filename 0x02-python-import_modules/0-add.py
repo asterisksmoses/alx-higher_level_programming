8dec0aa78eee7e570cbd7f81fbc2fe46f2b15a71
asterisksmoses <asterisksmoses@gmail.com>
2024-03-14 04:57:42 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as add0
-a = 1
-b = 2
-result = add0.add(a, b)
-print(f"{a} + {b} = {result}")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
