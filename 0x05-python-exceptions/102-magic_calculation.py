866b3e41bfa671a9c46e0662e127e7e757c1c7f7
asterisksmoses <asterisksmoses@gmail.com>
2024-04-02 14:12:43 -0700

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    res = 0
+    for y in range(1, 3):
+        try:
+            if y > a:
+                raise Exception("Too far")
+            else:
+                res += a ** b / y
+        except Exception:
+            res = b + a
+            break
+    return res
