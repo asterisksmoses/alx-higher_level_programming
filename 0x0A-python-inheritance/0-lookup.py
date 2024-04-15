d0de798da90ce2a026a7829fdad4587277d8f07b
asterisksmoses <asterisksmoses@gmail.com>
2024-04-15 04:51:45 -0700

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    A function that takes an object as an argument amd returns the a
+    list of the attributes and methods that are available on the object
+
+    Parameters:
+    obj : The object for which to list variable methods and attributes
+
+    Returns: A list of strings representing the names of attributes
+    and methods available on the object.
+
+    """
+    return dir(obj)
