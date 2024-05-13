08c6211ea18ceb36ace7031bfae69d74144b0857
asterisksmoses <asterisksmoses@gmail.com>
2024-05-13 06:37:46 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A square module."""
+
+
+class Square:
+    """A class to represent a Square."""
+    def __init__(self, size=0):
+        """Constructor
+
+
+        Args:
+        size (int): The length of one side of the square
+        """
+        self.size = size
+
+
+    def size(self):
+        """A Getter way to retrieve the size of the square."""
+        return self.__size
+
+
+    def size(self, value):
+        """A size setter method the set the size of the square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Finds the area of the square"""
+        return self.size ** 2
