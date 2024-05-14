7715d31ef52daf352b175c5effbd7a7fd00832d1
asterisksmoses <asterisksmoses@gmail.com>
2024-05-14 01:59:27 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,22 +11,24 @@ class Square:
         Args:
         size (int): The length of one side of the square
         """
-        self.size = size
+        self.__size = int(size)
 
 
-    def size(self):
+    def get_size(self):
         """A Getter way to retrieve the size of the square."""
         return self.__size
 
 
-    def size(self, value):
+    def set_size(self, value):
         """A size setter method the set the size of the square."""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
+
+    size = property(get_size, set_size)
 
     def area(self):
         """Finds the area of the square"""
-        return self.size ** 2
+        return self.__size ** 2
