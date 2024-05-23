98f84ab270f751c2d685032da35df72e19d71a88
asterisksmoses <asterisksmoses@gmail.com>
2024-05-23 04:29:27 -0700

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -12,3 +12,5 @@ INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (2, 'Alex', 3);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (3, 'Bob', 14);
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (4, 'George', 8);
 
+SELECT * FROM hbtn_0c_0.second_table;
+
