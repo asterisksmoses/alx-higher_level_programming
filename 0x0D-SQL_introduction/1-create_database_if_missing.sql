839e7b65f30fd05266e45cd9a2588d89fbaddb86
asterisksmoses <asterisksmoses@gmail.com>
2024-05-23 00:47:37 -0700

    1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -2,4 +2,4 @@
 -- The script creates a database hbtn_0c_0
 -- If the datavbase exists my script should not fail
 
-CREATE DATABASE IF NOT EXISTS hbtni_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
