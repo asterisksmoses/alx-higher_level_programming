8b412a0727110f2c6e5bae8613915cee03a87fed
asterisksmoses <asterisksmoses@gmail.com>
2024-05-23 02:41:15 -0700

    6-list_values.sql

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,7 @@
+-- Connect to the MySQL server with enough privileges
+-- The script lists all the all rows of the table first_table from the database hbtn_0c_0
+
+USE hbtn_0c_0;
+
+SELECT * FROM first_table;
+
