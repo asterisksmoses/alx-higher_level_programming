1768747e884f5f0c6e71faa035405c131ffbe104
asterisksmoses <asterisksmoses@gmail.com>
2024-05-23 01:41:44 -0700

    5-full_table.sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+-- Connect to the MySQL server with enough privileges
+-- Lists the full contents of of the table first_table from the database hbtn_0c_0 in your MySQL server
+
+USE hbtn_0c_0;
+
+SHOW CREATE TABLE first_table;
+
