e5c53039cafebd1e0274cb4ee05bf58e19f2ab7a
asterisksmoses <asterisksmoses@gmail.com>
2024-05-23 00:44:52 -0700

    1-create_database_if_missing.sql

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Connect to the MySQL server with enough privileges
+-- The script creates a database hbtn_0c_0
+-- If the datavbase exists my script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtni_0c_0;
