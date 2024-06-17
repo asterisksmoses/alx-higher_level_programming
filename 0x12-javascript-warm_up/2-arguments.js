5c659727c437df5d8a8f8e5d967547750e624792
asterisksmoses <asterisksmoses@gmail.com>
2024-06-17 12:18:19 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const args = 0;
+const args = process.argv.slice(2);
 
-if (args === 0) {
+if (args.length === 0) {
   console.log('No arguments');
-} else if (args === 1) {
+} else if (args.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
