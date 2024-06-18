fe4d6e966ee61e2b574931fe5cb624102cebe238
asterisksmoses <asterisksmoses@gmail.com>
2024-06-18 01:59:08 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+const size = parseInt(args[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let x = 0; x < size; x++) {
+    let row = '';
+    for (let y = 0; y < size; y++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
