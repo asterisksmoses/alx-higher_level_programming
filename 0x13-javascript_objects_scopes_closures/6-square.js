50c9340d07588aa52833a50b2d559d29ce47c4c4
asterisksmoses <asterisksmoses@gmail.com>
2024-06-19 14:37:14 -0700

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,12 @@
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
-	charPrint(c) {
-		const char = c === undefined ? 'X' : c;
-		for (let x = 0; x < this.height; x++) {
-			console.log(char.repeat(this.width));
-		}
-	}
+  charPrint (c) {
+    const char = c === undefined ? 'X' : c;
+    for (let x = 0; x < this.height; x++) {
+      console.log(char.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Square;
