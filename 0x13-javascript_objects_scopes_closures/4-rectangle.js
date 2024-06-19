1c2d93ed0be1772395a09e8763f8f1512e60c76f
asterisksmoses <asterisksmoses@gmail.com>
2024-06-18 23:49:48 -0700

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let x = 0; x < this.height; x++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
