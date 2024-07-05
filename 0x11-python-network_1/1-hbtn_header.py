da2d26be3e2ad3c5e9d801fcd444b0f19c495d2c
asterisksmoses <asterisksmoses@gmail.com>
2024-07-05 04:23:46 -0700

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,23 +6,5 @@ import sys
 URL interface and error handling."""
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
-        url = sys.argv[1]
-
-        try:
-            with urllib.request.urlopen(url) as response:
-                req_id = response.getheader("X-Request-Id")
-
-            if req_id:
-                print(f"X-Request-Id: {x_request_id}")
-            else:
-                print("X-Request-Id header not found in the response.")
-
-        except urllib.error.URLError as e:
-            print(f"URLError: {e.reason}")
-
-        except Exception as e:
-            print(f"Error: {e}")
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers['X-Request-Id'])
