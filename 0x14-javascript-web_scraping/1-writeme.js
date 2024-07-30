1870f452a3e58665f8dde80b5df927a4c996b737
asterisksmoses <asterisksmoses@gmail.com>
2024-07-30 04:46:48 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const process = require('process');
+
+const file_Path = process.argv[2]
+const string_To_Write = process.argv[3]
+
+if (!file_Path || !string_To_Write) {
+	console.error('Error: Both file path and string to write are not available');
+	process.exit(1);
+}
+
+fs.writeFile(file_Path, string_To_Write, 'utf8', (err) => {
+	if (err) {
+		console.error('Error occured while writing to file:', err);
+		return;
+	}
+	console.log('Successfully wrote to $file_Path');
+});
