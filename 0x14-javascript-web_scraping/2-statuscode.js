d06c0a5726206c4c7dfda4799a6d5c0259777b6c
asterisksmoses <asterisksmoses@gmail.com>
2024-07-30 05:16:50 -0700

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const process = require('process');
+
+const url = process.argv[2];
+
+if (!url) {
+	console.error('Error: No URL available');
+	process.exit(1);
+}
+
+request.get(url, (error, response, body) => {
+	if (error) {
+		console.error('Error in making a request:', error);
+		return;
+	}
+
+	console.log('code: ${response.statusCode}');
+});
